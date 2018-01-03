4874a92787c0976097012e1997838a4aa55fa2a1
johnluchn <johnluchn@gmail.com>
2018-01-03 13:26:33 +0800

    前面首页、列表页、文章页模板导入

---
+++ app/Http/Controllers/Home/CommonController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+
+class CommonController extends Controller
+{
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,18 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace'=>'Home'],function () {
+    Route::get('/','IndexController@index');
+    Route::get('/cate','IndexController@cate');
+    Route::get('/article','IndexController@article');
 });
 
 Route::group(['prefix'=>'admin','namespace'=>'Admin'], function () {
