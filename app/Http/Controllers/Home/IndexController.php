a07ffeb424fcbf023da4f85121887777b1483053
johnluchn <johnluchn@gmail.com>
2018-01-04 00:02:32 +0800

    前台文章图文列表、热读文章排行及分页实现

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Http\Model\Article;
+use App\Http\Model\Links;
 use App\Http\Model\Navs;
 use Illuminate\Http\Request;
 
@@ -11,7 +13,22 @@ class IndexController extends CommonController
 {
     public function index ()
     {
-        return view('home.index');
+        // 点击量最高的6篇文章
+        $hotArticle = Article::orderBy('art_view','des')->take(6)->get();
+
+        // 图文列表前5篇（带分页）
+        $data = Article::orderBy('art_time','des')->paginate(5);
+
+        // 最新文章
+        $newArticle = Article::orderBy('art_view','des')->take(8)->get();
+
+        // 随机文章
+        $randomArticle = Article::orderBy('art_id','des')->get()->random(5);
+
+        // 友情链接
+        $links = Links::orderBy('link_order','asc')->get();
+
+        return view('home.index',compact('hotArticle','data','newArticle','randomArticle','links'));
     }
 
     public function cate()
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -20,15 +20,12 @@
 <div class="template">
   <div class="box">
     <h3>
-      <p><span>个人博客</span>模板 Templates</p>
+      <p><span>热读文章</span> Hot article</p>
     </h3>
     <ul>
-      <li><a href="/"  target="_blank"><img src="{{asset('home/images/01.jpg')}}"></a><span>仿新浪博客风格·梅——古典个人博客模板</span></li>
-      <li><a href="/" target="_blank"><img src="{{asset('home/images/02.jpg')}}"></a><span>黑色质感时间轴html5个人博客模板</span></li>
-      <li><a href="/"  target="_blank"><img src="{{asset('home/images/03.jpg')}}"></a><span>Green绿色小清新的夏天-个人博客模板</span></li>
-      <li><a href="/" target="_blank"><img src="{{asset('home/images/04.jpg')}}"></a><span>女生清新个人博客网站模板</span></li>
-      <li><a href="/"  target="_blank"><img src="{{asset('home/images/02.jpg')}}"></a><span>黑色质感时间轴html5个人博客模板</span></li>
-      <li><a href="/"  target="_blank"><img src="{{asset('home/images/03.jpg')}}"></a><span>Green绿色小清新的夏天-个人博客模板</span></li>
+      @foreach($hotArticle as $h)
+        <li><a href="{{ url('article/'.$h->art_id) }}"  target="_blank"><img src="{{ '/uploads/'.$h->art_thumb }}"></a><span>{{ $h->art_title }}</span></li>
+      @endforeach
     </ul>
   </div>
 </div>
@@ -37,42 +34,24 @@
     <p>文章<span>推荐</span></p>
   </h2>
   <div class="bloglist left">
-    <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-    <figure><img src="{{asset('home/images/001.png')}}"></figure>
-    <ul>
-      <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-      <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-    </ul>
-    <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-    <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-    <figure><img src="{{asset('home/images/001.png')}}"></figure>
-    <ul>
-      <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-      <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-    </ul>
-    <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-    <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-    <figure><img src="{{asset('home/images/001.png')}}"></figure>
-    <ul>
-      <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-      <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-    </ul>
-    <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-    <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-    <figure><img src="{{asset('home/images/001.png')}}"></figure>
-    <ul>
-      <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-      <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-    </ul>
-    <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
-    <h3>程序员请放下你的技术情节，与你的同伴一起进步</h3>
-    <figure><img src="{{asset('home/images/001.png')}}"></figure>
-    <ul>
-      <p>如果说掌握一门赖以生计的技术是技术人员要学会的第一课的话， 那么我觉得技术人员要真正学会的第二课，不是技术，而是业务、交流与协作，学会关心其他工作伙伴的工作情况和进展...</p>
-      <a title="/" href="/" target="_blank" class="readmore">阅读全文>></a>
-    </ul>
-    <p class="dateview"><span>2013-11-04</span><span>作者：后盾</span><span>个人博客：[<a href="/news/life/">程序人生</a>]</span></p>
+
+    {{--文章列表--}}
+    @foreach($data as $d)
+      <h3>{{ $d->art_title }}</h3>
+      <figure><img src="{{ 'uploads/'.$d->art_thumb }}" width="200" height="120"></figure>
+      <ul>
+        <p>{{ $d->art_description }}</p>
+        <a title="{{ $d->art_title }}" href="{{ url('article/'.$d->art_id) }}" target="_blank" class="readmore">阅读全文>></a>
+      </ul>
+      <p class="dateview">&nbsp;<span>{{ date('Y-m-d H:i',$d->art_time) }}</span><span>作者：{{ $d->art_editor }}</span></p>
+    @endforeach
+
+    {{--分页--}}
+      <div class="page">
+        {{ $data->links() }}
+      </div>
   </div>
+
   <aside class="right">
     <div class="weather"><iframe width="250" scrolling="no" height="60" frameborder="0" allowtransparency="true" src="http://i.tianqi.com/index.php?c=code&id=12&icon=1&num=1"></iframe></div>
     <div class="news">
